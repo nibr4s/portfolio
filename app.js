2770f01776fda6f882c2c7966806e7dd71679284
nibr4s <nibras.bouarroudj@latoile.em-lyon.com>
2025-04-15 17:23:31 +0200

    3

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-        document.querySelector("#logo").style.display = "none"
-    }, 5000)
-   
+
+document.addEventListener("DOMContentLoaded",()=>{
+    setTimeout(()=>{
+        const test = document.querySelector("spline-viewer")
+    const logo = test.shadowRoot.querySelector("#logo")
+    logo.style.display = "none"
+    },1000)
 })
